74b0ecd56ef880150538d22198f968604c4c3b57
Minix Igor <migorec@gmail.com>
2020-07-17 19:46:46 +0300

    starter kit refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -3,17 +3,27 @@ import Network.HTTP.Simple
 import Data.ByteString.Lazy.UTF8 as BLU
 import Control.Exception
 
+
+send url msg = do
+    request' <- parseRequest ("POST " ++ url)
+    putStrLn $ "Sending message: " ++ msg
+    let request = setRequestBodyLBS (BLU.fromString msg) request'
+    response <- httpLBS request
+    let statuscode = show (getResponseStatusCode response)
+        strResp = BLU.toString (getResponseBody response)
+    case statuscode of
+        "200" -> putStrLn ("Server response: " ++ strResp)
+        _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ strResp)
+    return strResp
+
 main = catch (
     do  
         args <- getArgs
         putStrLn ("ServerUrl: " ++ args!!0 ++ "; PlayerKey: " ++ args!!1)
-        request' <- parseRequest ("POST " ++ (args!!0))
-        let request = setRequestBodyLBS (BLU.fromString (args!!1)) request'
-        response <- httpLBS request
-        let statuscode = show (getResponseStatusCode response)
-        case statuscode of
-            "200" -> putStrLn ("Server response: " ++ BLU.toString (getResponseBody response))
-            _ -> putStrLn ("Unexpected server response:\nHTTP code: " ++ statuscode ++ "\nResponse body: " ++ BLU.toString (getResponseBody response))
+        
+        r <- send (args!!0) (args!!1)
+        return ()
+
     ) handler
     where
         handler :: SomeException -> IO ()
