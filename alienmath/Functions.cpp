0c2fcbe8a19bedb2c7a9a81a97dd4f1d2f564a46
Ilia Lilov <lilovil@gmail.com>
2020-07-18 19:37:14 +0300

    Functions.h: all "g" function classes declared.

--- alienmath/Functions.cpp
+++ alienmath/Functions.cpp
@@ -1,1 +1,12 @@
 #include "Functions.h"
+
+NodePtr Sum::evalFull() const
+{
+	NodePtr x0 = m_args[0]->eval();
+	NodePtr x1 = m_args[1]->eval();
+	assert(x0->type() == Type::Int);
+	assert(x1->type() == Type::Int);
+	return NodePtr(static_cast<const Node *>(new Int(
+		dynamic_cast<const Int *>(x0.get())->m_value
+		+ dynamic_cast<const Int *>(x1.get())->m_value)));
+}
--- alienmath/Functions.h
+++ alienmath/Functions.h
@@ -9,16 +9,133 @@ public:
 	Sum() {}
 	virtual Type type() const override { return Type::Sum; }
 	virtual unsigned maxArgs() const override { return 2; }
-	virtual NodePtr evalFull() const override
-	{
-		NodePtr x0 = m_args[0]->eval();
-		NodePtr x1 = m_args[1]->eval();
-		assert(x0->type() == Type::Int);
-		assert(x1->type() == Type::Int);
-		return NodePtr(static_cast<const Node *>(new Int(
-			dynamic_cast<const Int *>(x0.get())->m_value
-			+ dynamic_cast<const Int *>(x1.get())->m_value)));
-	}
+	virtual NodePtr evalFull() const override;
+};
+
+class Product: public Function
+{
+public:
+	Product() {}
+	virtual Type type() const override { return Type::Product; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class IntDivision: public Function
+{
+public:
+	IntDivision() {}
+	virtual Type type() const override { return Type::IntDivision; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Equality: public Function
+{
+public:
+	Equality() {}
+	virtual Type type() const override { return Type::Equality; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class True: public Function
+{
+public:
+	True() {}
+	virtual Type type() const override { return Type::True; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Less: public Function
+{
+public:
+	Less() {}
+	virtual Type type() const override { return Type::Less; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Negate: public Function
+{
+public:
+	Negate() {}
+	virtual Type type() const override { return Type::Negate; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class CombS: public Function
+{
+public:
+	CombS() {}
+	virtual Type type() const override { return Type::CombS; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class CombC: public Function
+{
+public:
+	CombC() {}
+	virtual Type type() const override { return Type::CombC; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class CombB: public Function
+{
+public:
+	CombB() {}
+	virtual Type type() const override { return Type::CombB; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class CombI: public Function
+{
+public:
+	CombI() {}
+	virtual Type type() const override { return Type::CombI; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Cons: public Function
+{
+public:
+	Cons() {}
+	virtual Type type() const override { return Type::Cons; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Car: public Function
+{
+public:
+	Car() {}
+	virtual Type type() const override { return Type::Car; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class Cdr: public Function
+{
+public:
+	Cdr() {}
+	virtual Type type() const override { return Type::Cdr; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
+};
+
+class IsNil: public Function
+{
+public:
+	IsNil() {}
+	virtual Type type() const override { return Type::IsNil; }
+	virtual unsigned maxArgs() const override { return 2; }
+	virtual NodePtr evalFull() const override;
 };
 
 #endif // FUNCTIONS_H
