a5307c529eb114859b13a44aa6892ec37f028d57
Ilia Lilov <lilovil@gmail.com>
2020-07-19 16:59:23 +0300

    Attempt to feed libQt5Core.so.5 file manually

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ ls -lah /lib/x86_64-linux-gnu | grep -i qtcore
 echo "/usr/include/x86_64-linux-gnu/qt5/QtCore/:"
 ls -lah /usr/include/x86_64-linux-gnu/qt5/QtCore/ | grep -i qcore
 
-cd app
-mkdir ../build
-cd ../build
+mkdir build
+cd build
 qmake ../app/qt_hello_world.pro
 make
+
+#ln -s ../app/libQt5Core.so.5 libQt5Core.so.5
 
 ldd qt_hello_world
 
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+cd app
+LD_LIBRARY_PATH=`pwd`
 cd build
+ldd qt_hello_world
 ./qt_hello_world "$@" || echo "run error code: $?"
+
