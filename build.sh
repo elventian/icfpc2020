b8ddbc669c48f67a0568fccafce6bfad574be5ff
Ilia Lilov <lilovil@gmail.com>
2020-07-19 16:35:03 +0300

    Debugging libQt5Core.so.5 problem 02

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "/lib/x86_64-linux-gnu/:"
 ls -lah /lib/x86_64-linux-gnu | grep -i qtcore
+
+echo "/usr/include/x86_64-linux-gnu/qt5/QtCore/:"
+ls -lah /usr/include/x86_64-linux-gnu/qt5/QtCore/ | grep -i qcore
 
 cd app
 mkdir ../build
