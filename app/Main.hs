1c9a45ea696bfbe1608fd351699ede2f5ae02b1b
Minix Igor <migorec@gmail.com>
2020-07-20 00:07:35 +0300

    shoot +v

--- app/Main.hs
+++ app/Main.hs
@@ -25,25 +25,27 @@ send url msg = do
     return $ fst$demodulate strResp
 
 
-findShId r (BPart "cons_2" [h,t]) = if role==r then (shid, pos) else findShId r t 
+findShId r (BPart "cons_2" [h,t]) = if role==r then (shid, pos, vel) else findShId r t 
     where role = evalBlock' $BApp (BName "car") $ BApp (BName "car") h
           shid = evalBlock' $BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "car") h
           pos = evalBlock' $BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "cdr") $ BApp (BName "car") h
+          vel = evalBlock' $BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "cdr") $ BApp (BName "cdr") $ BApp (BName "car") h
 findShId r s = error $ show s
 
 loop r url key  = do
     let stage = evalBlock' $ BApp (BName "car") $ BApp (BName "cdr") r
     let role = evalBlock' $ BApp (BName "car") $ BApp (BName "cdr") $BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "cdr") r
     let shipsnadcommands = evalBlock' $ BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "cdr") $BApp (BName "car") $ BApp (BName "cdr") $ BApp (BName "cdr") $ BApp (BName "cdr") r
-    let (shid, pos) = findShId role shipsnadcommands
+    let (shid, pos, vel) = findShId role shipsnadcommands
     let (BPart "cons_2" [BNum x, BNum y]) = pos
     let (BNum rv) = role
-    let (eid, epos) = findShId (BNum (1-rv)) shipsnadcommands
+    let (eid, epos, evel) = findShId (BNum (1-rv)) shipsnadcommands
     let (BPart "cons_2" [BNum ex, BNum ey]) = epos
+    let (BPart "cons_2" [BNum evx, BNum evy]) = evel
     if stage == BNum 2
     then return ()
     else do let BMod mes = evalBlock' $  BApp (BName "mod") $ BList [BNum 4, BNum key, BList[BList[BNum 0, shid, (BPart "cons_2" [BNum (if abs x > abs y then (-x `div` (abs x)) else 0),BNum (if abs y > abs x then (-y `div` (abs y)) else 0)])],
-                                                                                             BList [BNum 2, shid, epos, BNum 5]
+                                                                                             BList [BNum 2, shid, BPart "cons_2" [BNum (ex+evx), BNum (ey+evy)] , BNum 5]
                                                                                             ]]
             r1 <- send url mes
             loop r1 url key
