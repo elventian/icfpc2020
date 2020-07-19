2ac03e109f49ae731369120600c883bbd09cd4ef
Valentine Lilova <elventian@gmail.com>
2020-07-19 21:39:18 +0300

    Shoot coord(0,0) x3 = 1

--- app/Game.cpp
+++ app/Game.cpp
@@ -30,8 +30,7 @@ void Game::run()
 	CommandList commands;
 	//commands.push_back(new Accelerate(1, DiscrCoord2(1, 1)));
 	//commands2.push_back(new Accelerate(1, DiscrCoord2(-1, -1)));
-	//commands.push_back(new Shoot(0, DiscrCoord2(50, 50), 0));
-	commands.push_back(new UnknownCommand(0, 0));
+	commands.push_back(new Shoot(0, DiscrCoord2(0, 0), 1));
 	for (int i = 0; i < maxTurns; i++) {
 		sendCommands(commands);
 	}
