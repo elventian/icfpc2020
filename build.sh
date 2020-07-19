5d5a6ac975bf14d11be24de41a692d5fcaec7c24
Ilia Lilov <lilovil@gmail.com>
2020-07-19 16:08:14 +0300

    Test if stdout of build.sh is visible in Docker log.

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 cd app
 mkdir ../build
+echo "Stdout is visible"
 g++ -std=c++11 -fPIC -lQt5Core -I/usr/include/x86_64-linux-gnu/qt5/QtCore/ -I/usr/include/x86_64-linux-gnu/qt5/ -o ../build/main main.cpp
