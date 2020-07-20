05971cfbcdf6c836f1f4ce03fa4aec9e76e510bc
Minix Igor <migorec@gmail.com>
2020-07-20 09:38:52 +0300

    try not to crash

--- app/Main.hs
+++ app/Main.hs
@@ -45,7 +45,8 @@ loop r url key  = do
     let (eid, epos, evel,BNum efuel,  _) = findShId (BNum (1-rv)) shipsnadcommands
     let (BPart "cons_2" [BNum ex, BNum ey]) = epos
     let (BPart "cons_2" [BNum evx, BNum evy]) = evel
-    let (ax, ay) = (-127 * signum ex, -127 * signum ey)
+    let (ax', ay') = (-127 * signum ex, -127 * signum ey)
+    let (ax, ay) = if (signum ax' == -1*signum x && signum ay' == -1*signum y) then (ax', -ay') else (ax,ay)
     let mCmd = if (abs x >= 127 || abs y >= 127) && signum x == signum ax && signum y == signum ay ||rv==0
                then BList[BNum 0, shid, (BPart "cons_2" [BNum (if abs x > abs y then (-x `div` (abs x)) else 0),BNum (if abs y > abs x then (-y `div` (abs y)) else 0)])]
                else BList[BNum 0, shid, (BPart "cons_2" [BNum (if signum  (x - ax) == -1 * signum vx && abs vx > 6 then 0 else signum $ x - ax),BNum (if signum (y - ay) == -1 * signum vy && abs vy > 6 then 0 else signum $ y - ay)])]
